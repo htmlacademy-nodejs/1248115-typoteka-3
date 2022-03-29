b975f4029cb645cee7a531de92fdf5ef4a69763a
Kapitonov Roman <kapitroman@yandex.ru>
2022-03-29 17:10:32 +0300

    шаблонизируй это

--- src/express/express.js
+++ src/express/express.js
@@ -1,11 +1,22 @@
 'use strict';
 
 const express = require(`express`);
-const mainRoutes = require(`./routes/main-routes`);
+const mainRoutes = require(`./routes/main-router`);
+const myRoutes = require(`./routes/my-router`);
+const articlesRoutes = require(`./routes/articles-router`);
+const path = require(`path`);
 
 const app = express();
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
 
 app.use(`/`, mainRoutes);
+app.use(`/my`, myRoutes);
+app.use(`/articles`, articlesRoutes);
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+app.use((req, res) => res.status(400).render(`errors/404`));
+
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 
 app.listen(DEFAULT_PORT);
